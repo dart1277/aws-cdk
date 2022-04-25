31a65578d154653300d8a0d6084ade47c873a11d
kornel <kornel.k.krawczyk@o2.pl>
2022-04-25 23:13:09 +0200

    add simple lambda with api gw

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
+import os
 
 import aws_cdk as cdk
 
-from cdk_sample.cdk_sample_stack import CdkSampleStack
-
+from cdk_sample.lambda_stack import LambdaStack
 
 app = cdk.App()
-CdkSampleStack(app, "cdk-sample")
+LambdaStack(app, "LambdaStack", env=cdk.Environment(account=os.environ["CDK_DEFAULT_ACCOUNT"],
+                                                    region=os.environ["CDK_DEFAULT_REGION"]))  # env={'region': 'us-east-1'})
 
 app.synth()
---
+++ cdk_sample/lambda_stack.py
@@ -0,0 +1,26 @@
+from constructs import Construct
+from aws_cdk import (
+    Duration,
+    Stack,
+    aws_iam as iam,
+    aws_sqs as sqs,
+    aws_sns as sns,
+    aws_sns_subscriptions as subs,
+    aws_lambda as _lambda,
+    aws_apigateway as apigw,
+)
+
+
+class LambdaStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        hello_function = _lambda.Function(self,
+                                          'WelcomeHAndler',
+                                          runtime=_lambda.Runtime.PYTHON_3_9,
+                                          code=_lambda.Code.from_asset('lambda-api'),
+                                          handler='welcome.handler'
+                                          )
+
+        apigw.LambdaRestApi(self, 'Endpoint', handler=hello_function)
